a5cf7e84e4966d713210d0edb4176c87f4857931
Felipe <balcaam@gmail.com>
2020-11-25 16:48:04 -0300

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get '/', to: 'pages#home'
-  root to: 'pages#ask'
-
-  # verb '/path', to: 'controller#action'
-  get '/answer', to: 'pages#answer'
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
