6f06f41f09f2a19f7f600922917a5b8ef2cc83a7
Felipe <balcaam@gmail.com>
2020-11-25 15:32:04 -0300

    test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  def ask
-    @user_asks = %w[nothing yet]
-    user_input = params[:user_ask]
-    # raise
-    @user_asks = @user_asks.select { |name| name.downcase == user_input.downcase } if user_input
-  end
-  
+  def ask; end
+
   def answer; end
 end
